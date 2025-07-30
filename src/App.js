79c7663e6f89e5871210805a628734e29179121b
nader-alger <nader_alger@hotmail.com>
2025-07-30 15:20:51 +0300

    up to model everything is ok

--- src/App.js
+++ src/App.js
@@ -1,22 +1,72 @@
 import { useState } from 'react';
 import './index.css';
 import Employee from './Components/employee';
-
+import {v4 as uuidv4} from 'uuid';
 
 
 function App() {
 //let role='';
 
 const [role,setRole]=useState('');
+const [employees,setEmployees]=useState(
+  [
+    {name: "nader",
+    role: "Engineer",
+    img: "https://images.pexels.com/photos/906052/pexels-photo-906052.jpeg"
+  }
+    ,
 
+{name: "Adam",
+    role: "manager",
+    img: "https://images.pexels.com/photos/1839564/pexels-photo-1839564.jpeg"
+
+  },
+{name: "Jomana",
+    role: "Cordinator",
+    img: "https://ix-marketing.imgix.net/focalpoint.png"}
+  ,
+
+  {name: "nader",
+    role: "Engineer",
+    img: "https://images.pexels.com/photos/906052/pexels-photo-906052.jpeg"
+  }
+    ,
+
+{name: "Adam",
+    role: "manager",
+    img: "https://images.pexels.com/photos/1839564/pexels-photo-1839564.jpeg"
+
+  },
+{name: "Jomana",
+    role: "Cordinator",
+    img: "https://ix-marketing.imgix.net/focalpoint.png"}
+
+
+  ]
+);
 return (
-    <div className="App bg-red-300">
+  <div className="App">
       <input type="text" onChange={(e)=>
         setRole(e.target.value)}></input>
-       <Employee name="Nader" role={role} />  
-       <Employee name="Adam" role="Programmer" /> 
-       <Employee name="Jomana" />
-    </div>
+       <div className="flex flex-wrap justify-center">
+          
+          {employees.map((employee)=>{
+          console.log(employee);
+          console.log(uuidv4());
+          return (
+            <Employee 
+              key={uuidv4()}
+              name={employee.name}
+              role={employee.role} 
+              img={employee.img}
+              />
+          )
+           
+})}
+          
+          
+       </div>
+    </div> 
   );
 }
 
--- src/Components/employee.js
+++ src/Components/employee.js
@@ -1,17 +1,25 @@
-/* function Employee() {
-    return
-    ( <>
-            <h3> nader fathi </h3>
-        </>
-    );
-} */
-
 function Employee(props) {
     return (
-        <>
-            <h3>Employee {props.name}</h3>
-            <h3>Role {props.role ? props.role : 'No Role'} </h3>
-        </>
+        <div className="m-2 py-8 px-8 max-w-sm space-y-2 bg-white rounded-xl shadow-lg sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:gap-x-6">
+            <img 
+                className="object-cover rounded-full h-[100px] w-[100px] block mx-auto h-24 rounded-full sm:mx-0 sm:shrink-0" 
+                src={props.img} 
+                //src="https://demo-source.imgix.net/model.jpg"
+                //src="https://ix-marketing.imgix.net/autotagging.png?auto=format,compress&w=1946"
+                alt="Woman's Face" 
+                />
+            <div className="text-center space-y-2 sm:text-left">
+                <div className="space-y-0.5">
+                    <p className="text-lg text-black font-semibold">{props.name}</p>
+                    <p className="text-slate-500 font-medium">{props.role}</p>
+                </div>
+                <button 
+                    className="px-4 py-1 text-sm text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2">
+                        Update
+                </button>
+            </div>
+        </div>
+
     );
 }
 
